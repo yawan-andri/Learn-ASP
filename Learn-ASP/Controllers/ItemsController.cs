b83dfeb72181db3a5b6de4b84f3183b30e424003
Andri <yawan.andrisetyawan@gmail.com>
2025-05-10 21:08:28 +0700

    Add project files.

---
+++ Learn-ASP/Controllers/ItemsController.cs
@@ -0,0 +1,19 @@
+﻿using Learn_ASP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learn_ASP.Controllers
+{
+	public class ItemsController : Controller
+	{
+		public IActionResult Overview()
+		{
+			var item = new Item() { Name = "Keyboard" };
+			return View(item);	
+		}
+
+		public IActionResult Edit(int id)
+		{
+			return Content("id = " + id);
+		}
+	}
+}
---
+++ Learn-ASP/Data/LearnASPContext.cs
@@ -0,0 +1,11 @@
+﻿using Learn_ASP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Learn_ASP.Data
+{
+	public class LearnASPContext : DbContext
+	{
+		public LearnASPContext(DbContextOptions<LearnASPContext> options) : base(options) { }
+		public DbSet<Item> Items { get; set; }
+	}
+}
