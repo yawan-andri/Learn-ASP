c6ab1c3c0c1726e634d04b71810b9a45a434712a
Andri <yawan.andrisetyawan@gmail.com>
2025-05-11 15:40:45 +0700

    update minggu

--- Learn-ASP/Controllers/ItemsController.cs
+++ Learn-ASP/Controllers/ItemsController.cs
@@ -1,19 +1,68 @@
-﻿using Learn_ASP.Models;
+﻿using Learn_ASP.Data;
+using Learn_ASP.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Learn_ASP.Controllers
 {
 	public class ItemsController : Controller
 	{
-		public IActionResult Overview()
+		private readonly LearnASPContext _context;
+		public ItemsController(LearnASPContext context)
 		{
-			var item = new Item() { Name = "Keyboard" };
-			return View(item);	
+			_context = context;
 		}
-
-		public IActionResult Edit(int id)
+		public async Task<IActionResult> Index()
 		{
-			return Content("id = " + id);
+			var items = await _context.Items.ToListAsync();
+			return View(items);
+		}
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Create([Bind("Id, Name, Price")] Item item)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(item);
+				await _context.SaveChangesAsync();
+				return RedirectToAction("Index");
+			} 
+			return View(item);
+		}
+		public async Task<IActionResult> Edit(int id)
+		{
+			var item = await _context.Items.FirstOrDefaultAsync(i => i.Id == id);
+			return View(item);
+		}
+		[HttpPost]
+		public async Task<IActionResult> Edit(int id, [Bind("Id, Name, Price")] Item item)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Update(item);
+				await _context.SaveChangesAsync();
+				return RedirectToAction("Index");
+			}
+			return View(item);
+		}
+		public async Task<IActionResult> Delete(int id)
+		{
+			var item = await _context.Items.FirstOrDefaultAsync(i => i.Id == id);
+			return View(item);
+		}
+		[HttpPost, ActionName("Delete")]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var item = await _context.Items.FindAsync(id);
+			if(item != null)
+			{
+				_context.Items.Remove(item);
+				await _context.SaveChangesAsync();
+			}
+			return RedirectToAction("Index");
 		}
 	}
 }
--- Learn-ASP/Migrations/20250511043200_second migration.Designer.cs
+++ Learn-ASP/Migrations/20250511043200_second migration.Designer.cs
@@ -3,6 +3,7 @@ using Learn_ASP.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Learn_ASP.Migrations
 {
     [DbContext(typeof(LearnASPContext))]
-    partial class LearnASPContextModelSnapshot : ModelSnapshot
+    [Migration("20250511043200_second migration")]
+    partial class secondmigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
